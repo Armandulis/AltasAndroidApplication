1c239191d2fab53d7cc2a1c5503d0a2c6dde53ec
Armandulis <Armandulis@gmail.com>
2019-11-23 01:56:38 +0100

    Removed edit functionality

--- app/src/main/java/com/example/altas/ui/about/AboutFragment.java
+++ app/src/main/java/com/example/altas/ui/about/AboutFragment.java
@@ -7,20 +7,15 @@ import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.altas.R;
-import com.example.altas.ui.about.edit.dialogs.EditAboutEmailDialogFragment;
-import com.example.altas.ui.about.edit.dialogs.EditAboutPhoneDialogFragment;
-import com.example.altas.ui.about.edit.dialogs.EditAboutUsDialogFragment;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 /**
@@ -94,33 +89,6 @@ public class AboutFragment extends Fragment {
             }
         });
 
-        // Get SendEmail button and add onClickListener
-        final Button buttonEditEmail = aboutFragmentRoot.findViewById(R.id.button_edit_about_email);
-        buttonEditEmail.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openEditEmailDialog();
-            }
-        });
-
-        // Get SendEmail button and add onClickListener
-        final Button buttonEditPhone = aboutFragmentRoot.findViewById(R.id.button_edit_about_phone);
-        buttonEditPhone.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openEditPhoneDialog();
-            }
-        });
-
-        // Get SendEmail button and add onClickListener
-        final Button buttonEditDescription = aboutFragmentRoot.findViewById(R.id.button_edit_about_us);
-        buttonEditDescription.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openEditAboutUsDialog();
-            }
-        });
-
         // Initialize Fragment's layout
         return aboutFragmentRoot;
     }
@@ -146,81 +114,5 @@ public class AboutFragment extends Fragment {
         Intent intent = new Intent(Intent.ACTION_DIAL);
         intent.setData(Uri.parse("tel:" + textViewAboutPhone.getText()));
         startActivity(intent);
-    }
-
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // Make sure fragment codes match up with wanted dialog data
-        if (requestCode == EditAboutEmailDialogFragment.REQUEST_CODE) {
-            String newEmail = data.getStringExtra(
-                    EditAboutEmailDialogFragment.NEW_EMAIL_KEY);
-            this.aboutViewModel.setAboutEmail(newEmail);
-        } else if (requestCode == EditAboutPhoneDialogFragment.REQUEST_CODE) {
-            String newPhone = data.getStringExtra(
-                    EditAboutPhoneDialogFragment.NEW_PHONE_KEY);
-            this.aboutViewModel.setAboutPhone(newPhone);
-        } else if (requestCode == EditAboutUsDialogFragment.REQUEST_CODE) {
-            String newAboutUs = data.getStringExtra(
-                    EditAboutUsDialogFragment.NEW_ABOUT_KEY);
-            this.aboutViewModel.setAboutUs(newAboutUs);
-        }
-    }
-
-    /**
-     * Opens EditEmailDialogFragment
-     */
-    private void openEditEmailDialog() {
-        // EditAboutEmailDialogFragment.show() will take care of adding the fragment
-        // in a transaction.  We also want to remove any currently showing
-        // dialog, so make our own transaction and take care of that here.
-        FragmentTransaction ft = getFragmentManager().beginTransaction();
-        Fragment prev = getFragmentManager().findFragmentByTag("editEmailDialog");
-        if (prev != null) {
-            ft.remove(prev);
-        }
-        ft.addToBackStack(null);
-
-        EditAboutEmailDialogFragment editAboutEmailDialogFragment = new EditAboutEmailDialogFragment();
-        editAboutEmailDialogFragment.setTargetFragment(this, EditAboutEmailDialogFragment.REQUEST_CODE);
-        editAboutEmailDialogFragment.show(ft, "editEmailDialog");
-    }
-
-    /**
-     * Opens EditPhoneDialogFragment
-     */
-    private void openEditPhoneDialog() {
-        // EditAboutPhoneDialogFragment.show() will take care of adding the fragment
-        // in a transaction.  We also want to remove any currently showing
-        // dialog, so make our own transaction and take care of that here.
-        FragmentTransaction ft = getFragmentManager().beginTransaction();
-        Fragment prev = getFragmentManager().findFragmentByTag("editPhoneDialog");
-        if (prev != null) {
-            ft.remove(prev);
-        }
-        ft.addToBackStack(null);
-
-        EditAboutPhoneDialogFragment editAboutPhoneDialogFragment = new EditAboutPhoneDialogFragment();
-        editAboutPhoneDialogFragment.setTargetFragment(this, EditAboutPhoneDialogFragment.REQUEST_CODE);
-        editAboutPhoneDialogFragment.show(ft, "editPhoneDialog");
-    }
-
-    /**
-     * Opens EditAboutUsDialogFragment
-     */
-    private void openEditAboutUsDialog() {
-        // EditAboutUsDialogFragment.show() will take care of adding the fragment
-        // in a transaction.  We also want to remove any currently showing
-        // dialog, so make our own transaction and take care of that here.
-        FragmentTransaction ft = getFragmentManager().beginTransaction();
-        Fragment prev = getFragmentManager().findFragmentByTag("editAboutUsDialog");
-        if (prev != null) {
-            ft.remove(prev);
-        }
-        ft.addToBackStack(null);
-
-        EditAboutUsDialogFragment editAboutUsDialogFragment = new EditAboutUsDialogFragment();
-        editAboutUsDialogFragment.setTargetFragment(this, EditAboutUsDialogFragment.REQUEST_CODE);
-        editAboutUsDialogFragment.show(ft, "editAboutUsDialog");
     }
 }
